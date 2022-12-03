75074c58be4a7491524f275df4f7ac187a47fb56
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-12-03 11:45:54 -0600

    Interfaces and their implementation

---
+++ login_interface.py
@@ -0,0 +1,13 @@
+class Login():
+    def login():
+        pass
+
+    def is_valid_email():
+        pass
+
+
+    def is_signup_sucess():
+        pass
+
+    def is_valid_password():
+        pass
---
+++ signup_Interface.py
@@ -0,0 +1,8 @@
+class SignupInterface():
+    def signup():
+        pass
+
+    def is_valid_email():
+        pass
+
+    
