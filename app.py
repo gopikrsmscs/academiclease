83a2546ad65f31719729b5223a8d470612194b4f
Gayathri Annem <annemgacs@gmail.com>
2022-12-04 21:45:48 -0600

    Added post url

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, redirect, request, session
+import email
+from flask import Flask, render_template, redirect, request, session,url_for,flash
 from flask_session import Session
 from database import database
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -10,17 +12,39 @@ Session(app)
 
 @app.route("/")
 def index():
-    if not session.get("uname"):
+    if not session.get("email"):
         return redirect("/login")
     return render_template('home.html')
+
+
+def get_post(post_id):
+    conn = database().get_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
  
  
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        username = request.form.get("uname")
+        email = request.form.get("email")
         password = request.form.get("password")
-        session["uname"] = request.form.get("uname")        
+        print(email)
+        query="select count(*) from user where email='"+email+"' and password='"+password+"')"
+        session['email'] = email
+        database_connection = database().get_connection()
+        mycursor = database_connection.cursor()
+        try:
+            mycursor.execute(query)
+            database_connection.commit()
+        except:
+            print("failed")
+            print(query)
+            database_connection.rollback()
+            database_connection.commit()
         return redirect("/")
     return render_template("login.html")
 
@@ -43,17 +67,69 @@ def signup():
             print(query)
             database_connection.rollback()
             database_connection.commit()
-        database_connection.close()
-        session["uname"] = request.form.get("uname") 
-        session["email"] = request.form.get("uname") 
-        session["id"] = request.form.get("uname")        
-        return redirect("/")
+        database_connection.close()       
+        return redirect("/login.html")
     return render_template("signup.html")
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('postid.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = database().get_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect('/')
+
+    return render_template('post.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = database().get_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect('/')
+
+    return render_template('editpost.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = database().get_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect('/')
 
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
-    session["uname"] = None
+    session["email"] = None
+    session["name"] = None
     return redirect("/")
 
 if __name__ == '__main__':
